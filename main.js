e19702668826bb5d97ae0c11c98d0c948fb5d3d3
Luis Miguel <68859191+luismsantos@users.noreply.github.com>
2021-10-22 23:29:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'luismsantos',
+  youtube: 'youtube',
+  facebook: 'luismiguel.santossouza',
+  instagram:'instagram',
+  twitter: 'luiwkk'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) { 
+   const social = li.getAttribute('class')
+
+   li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks() 
+
+function getGithubProfileInfos  () {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url).then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userPhoto.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+} 
+
+getGithubProfileInfos()
